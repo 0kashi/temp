2bfcc000d1d6afa4abee1684509a195284bf6350
0kashi <55109335+0kashi@users.noreply.github.com>
2021-11-19 12:48:18 -0800

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -7,7 +7,7 @@ sudo yum update -y
 #sudo rm -f /usr/bin/aws
 
 #install jq
-sudo yum install jq
+sudo yum install jq -y
 
 #install the new version 2 aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
